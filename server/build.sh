ce1f9a39da8355e77b2b58da0f5d4e8ef75ae3c2
chrisS41 <swseok94@gmail.com>
2024-11-21 00:46:08 +0900

    빌드 시 버전정보 등 저장 관련 오류 수정 빌드파일 ignore 처리

--- server/build.sh
+++ server/build.sh
@@ -3,5 +3,7 @@ VERSION=$(git describe --tags --always --dirty)
 REVISION=$(git rev-parse --short HEAD)
 go build \
     -mod=vendor \
-    -ldflags "-X main.date=$DATE -X main.version=$VERSION -X main.revision=$REVISION" \
+    -ldflags "-X github.com/chrisS41/gobike-server/internal/version.Version=$VERSION \
+              -X github.com/chrisS41/gobike-server/internal/version.Revision=$REVISION \
+              -X github.com/chrisS41/gobike-server/internal/version.BuildDate=$DATE" \
     -o gobike-server ./cmd/main.go
